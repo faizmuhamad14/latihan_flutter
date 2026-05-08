7c448d70806ff49a99b9fa4674cafd48011dd06b
faizmuhamad14 <muhamadfaiz9d@gmail.com>
2026-05-07 18:57:56 -0700

    Progress day 2 Tugas 1 flutter

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,34 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.blue[200],
-        appBar: AppBar(title: Text("Profil Saya")),
-        body: Column(
-          children: [
-            Row(children: [Text("Muhamad Faiz")]),
-            Row(children: [Icon(Icons.location_on), Text("Jakarta Selatan")]),
-            Row(
-              children: [
-                Text("Halo saya dipanggil Faiz, saya peserta PPKD Jakpus"),
-              ],
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("Profil Saya"),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(23.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                    "Muhamad Faiz",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ],
+              ),
+              Row(children: [Icon(Icons.location_on), Text("Jakarta Selatan")]),
+              Row(
+                children: [
+                  Text(
+                    "Halo saya dipanggil Faiz, saya peserta PPKD Jakpus",
+                    style: TextStyle(fontSize: 13, color: Colors.red[600]),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
