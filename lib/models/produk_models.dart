18ba5f55cd6ef52c245f993a577bc344aaf2cb22
faizmuhamad14 <muhamadfaiz9d@gmail.com>
2026-05-24 21:15:09 -0700

    Tugas 9 Flutter

---
+++ lib/models/produk_models.dart
@@ -0,0 +1,13 @@
+class ProdukPetshop {
+  final String nama;
+  final String kategori;
+  final int harga;
+  final String gambar;
+
+  ProdukPetshop({
+    required this.nama,
+    required this.kategori,
+    required this.harga,
+    required this.gambar,
+  });
+}
